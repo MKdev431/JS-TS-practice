435cdb59e6637a5ca46c1a01f78d86f4e055276b
Michal Kurek (Mike) <michal@kurek.pl>
2023-10-26 18:07:30 +0200

    add append function

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,22 @@
-console.log("hello23");
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = ["Take out the trash", "Go to the gym", "Feed the dog"];
+const render = () => {
+    tasksContainerElement.innerHTML = "";
+    tasks.forEach(task => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        tasksContainerElement.appendChild(taskElement);
+    });
+};
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskNameInputElement.value);
+    taskNameInputElement.value = "";
+    render();
+});
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("hello23");
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button");
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
+
+const tasks: string[] = ["Take out the trash", "Go to the gym", "Feed the dog"];
+
+const render = () => {
+  tasksContainerElement.innerHTML = "";
+  tasks.forEach(task => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.innerText = task;
+    tasksContainerElement.appendChild(taskElement);
+  });
+};
+
+const addTask = (task: string) => {
+  tasks.push(task);
+};
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  addTask(taskNameInputElement.value);
+  taskNameInputElement.value = "";
+  render();
+});
+
+render();
